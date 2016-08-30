5c1e4b47872ab6be3965f2870ef6a1c847c1de46
VladimirJava <spoolsv1@mail.ru>
2016-08-30 11:51:44 +0600

    Create program QuickSortThree Быстрая сортировка с применением сортировки методом вставки

---
+++ src/main/java/QuickSortThree/QuickSortThreeApp.java
@@ -0,0 +1,16 @@
+package QuickSortThree;
+
+public class QuickSortThreeApp {
+    public static void main(String[] args) {
+        int maxSize = 16;
+        ArrayIns arr = new ArrayIns(maxSize);
+
+        for (int j = 0; j < maxSize; j++) {
+            long n = (int) (java.lang.Math.random() * 99);
+            arr.insert(n);
+        }
+        arr.display();
+        arr.quickSort();
+        arr.display();
+    }
+}
